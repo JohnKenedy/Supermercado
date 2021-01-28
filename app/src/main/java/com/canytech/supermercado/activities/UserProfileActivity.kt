712b2e4fa88f51d3984b22851fe8016837e8b8cd
John Kenedy <john.kenedy0410@gmail.com>
2021-01-28 17:38:21 -0300

    Added: Read Permission in Profile Photo

--- app/src/main/java/com/canytech/supermercado/activities/UserProfileActivity.kt
+++ app/src/main/java/com/canytech/supermercado/activities/UserProfileActivity.kt
@@ -1,8 +1,12 @@
 package com.canytech.supermercado.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.canytech.supermercado.R
 import com.canytech.supermercado.models.User
 import com.canytech.supermercado.utils.Constants
@@ -25,8 +29,50 @@ class UserProfileActivity : BaseActivity(), View.OnClickListener {
         text_view_user_profile_number.text = userDetails.mobile.toString()
         text_view_user_profile_address.text = userDetails.address
 
+        profile_image.setOnClickListener(this@UserProfileActivity)
+
     }
 
     override fun onClick(v: View?) {
+
+        if (v != null) {
+            when (v.id) {
+
+                R.id.profile_image -> {
+
+                    if (ContextCompat.checkSelfPermission(
+                            this, Manifest.permission.READ_EXTERNAL_STORAGE
+                        )
+                        == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        showErrorSnackBar("You already have the storage permission.", false)
+                    } else {
+                        ActivityCompat.requestPermissions(
+                            this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                            Constants.READ_STORAGE_PERMISSION_CODE
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Constants.READ_STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                showErrorSnackBar("The permission is granted.", false)
+            } else {
+                Toast.makeText(
+                    this, resources.getString(R.string.storage_permissiob_denied),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
     }
 }
